7ed8d12ac8c2f00a1f04b917c466a9edb223d420
SUYASH NIKAM <nsuyash67@gmail.com>
2025-11-05 12:28:17 +0530

    Implement Distance class with operator overloading

---
+++ exp8.2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+class Distance 
+{
+public:
+    int feet, inch;
+
+    Distance() 
+	{
+        this->feet = 0;
+        this->inch = 0;
+    }
+
+    Distance(int f, int i) 
+	{
+        this->feet = f;
+        this->inch = i;
+    }
+
+    Distance operator+(Distance& d2) 
+	{
+        Distance d3;
+        d3.feet = this->feet + d2.feet;
+        d3.inch = this->inch + d2.inch;
+
+        if (d3.inch >= 12) 
+		{
+            d3.feet += d3.inch / 12;
+            d3.inch = d3.inch % 12;
+        }
+
+        return d3;
+    }
+};
+
+int main() 
+{
+
+    cout << "Name   : Suyash nikam " << endl;
+    cout << "Div    : B" << endl;
+    cout << "Roll No: 23" << endl;
+    cout << "-----------------------------" << endl;
+
+    Distance d1(8, 9);
+    Distance d2(10, 2);
+    Distance d3;
+
+    d3 = d1 + d2;
+
+    cout << "\nTotal Feet & Inches: " << d3.feet << "'" << d3.inch << endl;
+    return 0;
+}
